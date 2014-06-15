0dbed63934e30bb7bd134933254c96c0ca2b1fd0
Adrian Sampson <adrian@radbox.org>
2014-06-15 20:39:08 +0100

    Training/testing split

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Type `make` to compile the FANN library and our client tools. Or if you want to do the steps individually:
     * `make fann` builds FANN with CMake
     * `make train` builds the `train` tool (which links against the FANN dynamic library)
+    * `make recall` builds the `recall` tool for testing (also links to FANN, of course)
--- nntune.py
+++ nntune.py
@@ -1,6 +1,8 @@
+from __future__ import print_function
 import subprocess
 import tempfile
 import os
+import random
 
 
 TRAIN_CMD = './train'
@@ -27,33 +29,93 @@ def train(datafile, topology, epochs=100, learning_rate=0.7):
     return fn
 
 
-def get_shape(data_fn):
-    """Get the number of inputs and number of outputs in the data file.
-    """
-    # Get at least the first three numbers in the file.
-    parts = []
-    with open(data_fn) as f:
-        for line in f:
-            parts += line.strip().split()
-            if len(parts) >= 3:
-                break
-
-    return int(parts[1]), int(parts[2])
-
-
 def recall(nnfn, datafn):
     rmse = shell([RECALL_CMD, nnfn, datafn])
     return float(rmse)
 
 
-def nntune(datafn):
-    ninputs, noutputs = get_shape(datafn)
-    nnfn = train(datafn, [ninputs, 64, 2, noutputs])
+def read_data(fn):
+    """Read a data file as a list of (input, output) pairs.
+    """
+    values = []
+    with open(fn) as f:
+        for line in f:
+            values += line.strip().split()
+    nsamples = int(values.pop(0))
+    inputdim = int(values.pop(0))
+    outputdim = int(values.pop(0))
+
+    pairs = []
+    pos = 0
+    for i in range(nsamples):
+        inputs = values[pos:pos + inputdim]
+        pos += inputdim
+        outputs = values[pos:pos + outputdim]
+        pos += outputdim
+        pairs.append(([float(n) for n in inputs], [float(n) for n in outputs]))
+
+    return pairs
+
+
+def dump_data(data, f):
+    """Dump a series of (input, output) vector pairs to a file.
+    """
+    print(len(data), file=f)
+    print(len(data[0][0]), file=f)
+    print(len(data[0][1]), file=f)
+    for x, y in data:
+        print(' '.join(str(n) for n in x), file=f)
+        print(' '.join(str(n) for n in y), file=f)
+
+
+def dump_data_to_temp(data):
+    """Dump the data to a temporary file. Return the filename.
+    """
+    fd, fn = tempfile.mkstemp()
+    f = os.fdopen(fd, 'w')
+    dump_data(data, f)
+    f.close()
+    return fn
+
+
+def divide_data(pairs, proportion=0.7):
+    """Given a data set (sequence of pairs), divide it into two parts.
+    Return two filenames.
+    """
+    split_point = int(len(pairs) * proportion)
+    random.shuffle(pairs)
+    data1, data2 = pairs[:split_point], pairs[split_point:]
+
+    fn1 = dump_data_to_temp(data1)
+    fn2 = dump_data_to_temp(data2)
+
+    return fn1, fn2
+
+
+def evaluate(datafn, hidden_topology):
+    # Read data.
+    pairs = read_data(datafn)
+    ninputs, noutputs = len(pairs[0][0]), len(pairs[0][1])
+    topology = [ninputs] + hidden_topology + [noutputs]
+
+    # Split into training and testing files.
+    trainfn, testfn = divide_data(pairs)
     try:
-        rmse = recall(nnfn, datafn)
-        print(rmse)
+        # Train.
+        nnfn = train(trainfn, topology)
+        try:
+            # Test.
+            return recall(nnfn, testfn)
+
+        finally:
+            os.remove(nnfn)
     finally:
-        os.remove(nnfn)
+        os.remove(trainfn)
+        os.remove(testfn)
+
+
+def nntune(datafn):
+    print(evaluate(datafn, [64, 2]))
 
 
 if __name__ == '__main__':
