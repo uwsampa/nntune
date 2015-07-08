7f615fe1ad578cc70b06e5412602a162210fa745
Thierry Moreau <moreau@cs.washington.edu>
2015-07-07 18:39:30 -0700

    fixing C99 bug

--- recall.c
+++ recall.c
@@ -11,14 +11,15 @@ int main(int argc, char **argv)
     struct fann *ann = fann_create_from_file(argv[1]);
     struct fann_train_data *data = fann_read_train_from_file(argv[2]);
 
+    unsigned i, j;
     unsigned errors;
     unsigned count = fann_length_train_data(data);
     unsigned num_outputs = fann_num_output_train_data(data);
     fann_type se = 0.0;
-    for(unsigned i = 0; i < count; ++i)
+    for(i = 0; i < count; ++i)
     {
         fann_type *calc_out = fann_run(ann, data->input[i]);
-        for (unsigned j = 0; j < num_outputs; ++j) {
+        for (j = 0; j < num_outputs; ++j) {
             fann_type value = calc_out[j];
             // printf("%f ", calc_out[j]);
             se += value * value;
