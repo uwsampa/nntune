25f510f52e09d25a1b68002c68adbeb4cf48f913
Thierry Moreau <moreau@uw.edu>
2015-11-17 14:17:51 -0500

    correcting labels

--- post-process.py
+++ post-process.py
@@ -44,7 +44,7 @@ def process(csvpath):
     palette = [ '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
     # Y-labels
-    yLabels = ["classification", "recall", "false pos"]
+    yLabels = ["classification", "false pos", "recall"]
 
     # Dump to CSV file
     csvData = [["config", "features", "MADD"] + yLabels]
