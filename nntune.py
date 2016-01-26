02c2d048e60708cb859add792819e0e51ee5eece
Thierry Moreau <moreau@uw.edu>
2016-01-26 12:45:14 -0500

    copyright statement

--- nntune.py
+++ nntune.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python
 
+####################################################################################
+## File Name: nntune.py
+## Authors: Adrian Sampson, Thierry Moreau
+## Email: asampson@uw.edu, moreau@uw.edu
+##
+## Copyright (c) 2012-2016 University of Washington
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without modification,
+## are permitted provided that the following conditions are met:
+## •       Redistributions of source code must retain the above copyright notice,
+##         this list of conditions and the following disclaimer.
+## •       Redistributions in binary form must reproduce the above copyright notice,
+##         this list of conditions and the following disclaimer in the documentation
+##         and/or other materials provided with the distribution.
+## •       Neither the name of the University of Washington nor the names of its
+##         contributors may be used to endorse or promote products derived from this
+##         software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+## IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR CONTRIBUTORS BE LIABLE FOR ANY
+## DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+## OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+## NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+## IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+####################################################################################
+
 from __future__ import print_function
 import subprocess
 import tempfile
@@ -310,14 +341,22 @@ def nntune_sequential(datafn, datafn2, testfn, prec, errormode, errortarget, epo
         # Add the madd/error pair for the ANN
         ann_fn = nndir+'/'+topo_str+'_rep'+str(rep)+'.nn'
         ann_list.append([madds, t["mse"], ann_fn])
-    logging.info('{}'.format(ann_list))
 
     # Find optimal configuration
+    found = False
+    best_ann = ann_list[0]
     for ann in sorted(ann_list):
+        if ann[1] < best_ann[1]:
+            best_ann = ann
         if ann[1] < errortarget:
             shutil.copyfile(ann[2], nnpath)
-            logging.info("{} with {} error meets target of {}".format(ann[2],  ann[1], errortarget))
+            logging.info("ANN config {} with {} error meets target of {}".format(ann[2],  ann[1], errortarget))
+            found = True
             break
+    if not found:
+        logging.warning("No ANN configurations met error target {} - selecting ANN with smallest error".format(errortarget))
+        logging.info("ANN config {} with {} error was chosen".format(best_ann[2],  best_ann[1], errortarget))
+        shutil.copyfile(best_ann[2], nnpath)
 
     # Dump to CSV
     with open(csvpath, 'wb') as f:
